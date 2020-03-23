58b579a4606d3de324928aed7fb108bbc3cd28fa
baderbintahir <badarbintahir@gmail.com>
2020-03-23 22:36:52 +0500

    added input field

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 
 class App extends Component{
 
+    state = { artistQuery: '' };
+
+    updateArtistQuery = event => {
+        console.log('event.target.value => ', event.target.value);
+        this.setState({ artistQuery: event.target.value });
+    }
+
+    searchArtist =() => {
+        console.log('this.state => ', this.state)
+    }
+
+    handleKeyPress = event => {
+        if(event.key === 'Enter'){
+            this.searchArtist();
+        }
+    }
+
     render(){
         return(
-            <div>React App</div>
+            <div>
+                <h2>Music Master</h2>
+                <input
+                    onChange={this.updateArtistQuery}
+                    placeholder="Search of an Artist"
+                    onKeyPress={this.handleKeyPress}
+                />
+                <button onClick={this.searchArtist}>Search</button>
+            </div>
         )
     }
 }
